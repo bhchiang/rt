54982069057e9582e5de76fe5f33fdedeb3ea285
Bryan Chiang <bryanhpchiang@gmail.com>
2020-12-26 12:30:57 -0800

    Finished sphere normals

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
+from IPython import embed
 import jax.numpy as jnp
-from jax import vmap, lax
+from jax import vmap, lax, jit
 
 from ray import Ray
 from vec3 import vec, unit, x, y, z
@@ -16,36 +17,48 @@ lower_left_corner = origin - (horizontal/2) - \
     (vertical/2) - vec(0, 0, FOCAL_LENGTH)
 
 # indices
-X, Y = jnp.meshgrid(jnp.arange(IMAGE_WIDTH), jnp.arange(IMAGE_HEIGHT))
-a = jnp.array(list(zip(X.ravel(), Y.ravel()))).reshape(
-    IMAGE_WIDTH, IMAGE_HEIGHT,  2)
-# print(a.shape)
+xs = jnp.arange(IMAGE_WIDTH)
+ys = jnp.arange(IMAGE_HEIGHT)
+us, vs = jnp.meshgrid(xs, ys)
+idxs = jnp.dstack((us, jnp.flip(vs)))
+
+# print(us)
+# print(jnp.flip(vs))
+# print(idxs[IMAGE_HEIGHT, 0])
+# print(idxs[0, IMAGE_WIDTH])
 
 
 def hit_sphere(center, radius, r):
     orig, dir = r
     a = dir.dot(dir)
-    b = 2 * (orig.dot(dir) + dir.dot(center))
+    b = 2 * (orig.dot(dir) - dir.dot(center))
     c = (orig - center).dot(orig - center) - radius**2
-    # eprint(a, b, c)
     d = b**2 - 4*a*c
-    # eprint(d)
-    return d > 0
+    t = (-b - jnp.sqrt(d)) / 2*a
+    # eprint(d, t)
+    return jnp.where(d > 0, t, -1)
 
 
 def color(r):
-    hit = vec(1, 0, 0)
+    center = vec(0, 0, -1)
+    # earliest intersection time
+    h_t = hit_sphere(center, 0.5, r)
+    # eprint(r.at(h_t))
+    n = unit(r.at(h_t) - center)
+    n_c = 0.5*(n + 1)
+    # eprint(n, jnp.linalg.norm(n), n_c, 255.99*n_c)
 
     unit_dir = unit(r.dir)
     t = 0.5 * (y(unit_dir) + 1)  # -1 < y < 1 -> 0 < t < 1
     bg = (1-t) * vec(1, 1, 1) + t*vec(0.5, 0.7, 1.0)
 
-    return jnp.where(hit_sphere(vec(0, 0, -1), 0.5, r), hit, bg)
+    return jnp.where(h_t > 0, n_c, bg)
 
 
 def trace(d):
-    i, jj = jnp.array(d, jnp.float32)  # i = width, j = height
-    j = IMAGE_HEIGHT - jj - 1  # (0, 0) at lower left corner
+    # (i, j) = (0, 0) is lower left corner
+    # (IMAGE_WIDTH, IMAGE_HEIGHT) is upper right
+    i, j = jnp.array(d, jnp.float32)
 
     u = i / (IMAGE_WIDTH-1)
     v = j / (IMAGE_HEIGHT-1)
@@ -60,10 +73,10 @@ def trace(d):
     return (255.99 * c).astype(jnp.int32)
 
 
-# trace(a[0, 0])
-# sys.exit()
+# embed()
+# print(idxs.shape)
 
-img = vmap(vmap(trace))(a)
+img = vmap(vmap(trace))(idxs)
 pxls = create_pixel_list(img)
 eprint(pxls[:10], pxls.shape)
 
